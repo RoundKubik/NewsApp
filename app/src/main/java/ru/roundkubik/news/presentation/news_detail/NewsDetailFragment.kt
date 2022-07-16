f909f1601637c5cfb7a8ca6440cf55ab7b748160
Kubov Nikita <kubov.a.nikita@gmail.com>
2022-07-16 11:14:20 +0300

    Added raw layout

--- app/src/main/java/ru/roundkubik/news/presentation/news_detail/NewsDetailFragment.kt
+++
@@ -1,6 +0,0 @@
-package ru.roundkubik.news.presentation.news_detail
-
-import androidx.fragment.app.Fragment
-
-class NewsDetailFragment : Fragment() {
-}
--- app/src/main/java/ru/roundkubik/news/presentation/news_detail/NewsViewModel.kt
+++
@@ -1,6 +0,0 @@
-package ru.roundkubik.news.presentation.news_detail
-
-import androidx.lifecycle.ViewModel
-
-class NewsViewModel : ViewModel() {
-}
